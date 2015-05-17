3c0d1d76017243a9c56a457d1e4ccf7a98c586d5
Daniel Dragan <bulk88@hotmail.com>
2015-05-17 14:18:06 -0400

    coredir parallelism
    -defer the mass copy of headers to COREDIR until XS build time
     and dont use COREDIR for building miniperl interp or perl interp, more
     thing to do in parallel this way and 1 less path to search by the CC
    -config_h.PL requires config.h to be in COREDIR, this XCOPY in CONFIGPM
     targ is where COREDIR is created if it doesnt exist already
    -remove a bunch of XCOPY'es, copy the headers in 1 place only, and dont
     copy them unless they change based on timestamps
    -the DYNALOADER dep already contains
     "..\make_ext.pl ..\lib\buildcustomize.pl $(CONFIGPM) $(HAVE_COREDIR)" dont
     specify them again in Extensions and Extensions_reonly, this probably
     reduces the size of dep graph in dmake, sincedmake explodes the graph and
     checks the outline of the graph/tree very inefficiently in parallel mode
     (architectural problem with dmake)
    -undo Time::HiRes in-core patch per TonyC's 2 line ppport.h idea
    -delete pdb files in root, otherwise switching VC version will make the
     next cl.exe error out that it can't write to the pdb file due to version
     numbers in the pdb file
    -reorder core .c files in a blend of VC and GCC time order, this reduces
     a 1:08 mins time to do "dmake -P9 ../perl522.dll" to 0:54 mins on 8 core
     with GCC, see details in #123867

--- Porting/Maintainers.pl
+++ Porting/Maintainers.pl
@@ -1216,8 +1216,6 @@ use File::Glob qw(:case);
     'Time::HiRes' => {
         'DISTRIBUTION' => 'ZEFRAM/Time-HiRes-1.9726.tar.gz',
         'FILES'        => q[cpan/Time-HiRes],
-        # CPAN release eventually
-        'CUSTOMIZED'   => [ 'HiRes.xs' ],
     },
 
     'Time::Local' => {
